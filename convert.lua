e3593a98326b23237a9e5dbe61a11ead0e581656
Tsun-Yi Yang <shamangary@hotmail.com>
2017-08-21 10:46:33 +0800

    Update convert.lua

--- convert.lua
+++ convert.lua
@@ -5,7 +5,7 @@ net = torch.load('model.t7')
 print(net)
 torch.save('net.t7',net)
 
-cmd = 'th torch/export.lua ./net.t7 {1,3,55,55}'
+cmd = 'th torch/export.lua ./net.t7 1 3 55 55'
 os.execute(cmd)
 
 cmd = 'python caffe/import.py'
